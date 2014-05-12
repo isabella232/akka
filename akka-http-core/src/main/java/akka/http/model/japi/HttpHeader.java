27887d4c4cae9dfefb87d187c89288d70228bc3e
Johannes Rudolph <johannes.rudolph@gmail.com>
2014-05-12 17:21:59 +0200

    =hco covert HttpHeader to an abstract class

--- akka-http-core/src/main/java/akka/http/model/japi/HttpHeader.java
+++ akka-http-core/src/main/java/akka/http/model/japi/HttpHeader.java
@@ -1,9 +1,9 @@
 package akka.http.model.japi;
 
-public interface HttpHeader {
-    String name();
-    String value();
-    String lowercaseName();
-    boolean is(String nameInLowerCase);
-    boolean isNot(String nameInLowerCase);
+public abstract class HttpHeader {
+    public abstract String name();
+    public abstract String value();
+    public abstract String lowercaseName();
+    public abstract boolean is(String nameInLowerCase);
+    public abstract boolean isNot(String nameInLowerCase);
 }
