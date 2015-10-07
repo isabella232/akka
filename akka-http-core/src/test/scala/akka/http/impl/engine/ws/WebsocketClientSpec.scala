68a465dab986a4808df0b8be1b806f78b2e63125
Johannes Rudolph <johannes.rudolph@gmail.com>
2015-10-07 19:58:38 +0200

    =htc modernize tests by using proper probes and special ByteStringSinkProbe

--- akka-http-core/src/test/scala/akka/http/impl/engine/ws/WebsocketClientSpec.scala
+++ akka-http-core/src/test/scala/akka/http/impl/engine/ws/WebsocketClientSpec.scala
@@ -6,13 +6,13 @@ package akka.http.impl.engine.ws
 
 import java.util.Random
 
-import akka.actor.ActorSystem
+import scala.concurrent.duration._
+
 import akka.http.ClientConnectionSettings
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.headers.{ ProductVersion, `User-Agent` }
 import akka.http.scaladsl.model.ws._
 import akka.http.scaladsl.model.Uri
-import akka.stream.ActorMaterializer
 import akka.stream.io._
 import akka.stream.scaladsl._
 import akka.stream.testkit.{ TestSubscriber, TestPublisher }
@@ -23,9 +23,7 @@ import akka.http.impl.util._
 
 class WebsocketClientSpec extends FreeSpec with Matchers with WithMaterializerSpec {
   "The client-side Websocket implementation should" - {
-    "establish a websocket connection when the user requests it" in new EstablishedConnectionSetup with ClientEchoes {
-
-    }
+    "establish a websocket connection when the user requests it" in new EstablishedConnectionSetup with ClientEchoes
     "reject invalid handshakes" - {
       "missing Sec-WebSocket-Accept hash" in pending
       "wrong Sec-WebSocket-Accept hash" in pending
@@ -35,42 +33,22 @@ class WebsocketClientSpec extends FreeSpec with Matchers with WithMaterializerSp
 
     "don't send out websocket frames before handshake was finished successfully" in pending
     "receive first frame in same chunk as HTTP upgrade response" in new TestSetup with ClientProbes {
-      expectWireData(
-        """GET /ws HTTP/1.1
-          |Upgrade: websocket
-          |Connection: upgrade
-          |Sec-WebSocket-Key: YLQguzhR2dR6y5M9vnA5mw==
-          |Sec-WebSocket-Version: 13
-          |Host: example.org
-          |User-Agent: akka-http/test
-          |
-          |""".stripMarginWithNewline("\r\n"))
+      expectWireData(UpgradeRequestBytes)
 
       val firstFrame = WSTestUtils.frame(Protocol.Opcode.Text, ByteString("fast"), fin = true, mask = false)
-      sendWireData(ByteString(
-        """HTTP/1.1 101 Switching Protocols
-          |Upgrade: websocket
-          |Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
-          |Server: akka-http/test
-          |Connection: upgrade
-          |
-          |""".stripMarginWithNewline("\r\n")) ++ firstFrame)
+      sendWireData(UpgradeResponseBytes ++ firstFrame)
 
-      messagesInSub.request(1)
-      messagesIn.expectNext(TextMessage("fast"))
+      messagesIn.requestNext(TextMessage("fast"))
     }
 
     "manual scenario client sends first" in new EstablishedConnectionSetup with ClientProbes {
-      netOutSub.request(10)
-
-      messagesOutSub.sendNext(TextMessage("Message 1"))
+      messagesOut.sendNext(TextMessage("Message 1"))
 
       expectMaskedFrameOnNetwork(Protocol.Opcode.Text, ByteString("Message 1"), fin = true)
 
       sendWSFrame(Protocol.Opcode.Binary, ByteString("Response"), fin = true, mask = false)
 
-      messagesInSub.request(1)
-      messagesIn.expectNext(BinaryMessage(ByteString("Response")))
+      messagesIn.requestNext(BinaryMessage(ByteString("Response")))
     }
     "client echoes scenario" in new EstablishedConnectionSetup with ClientEchoes {
       sendWSFrame(Protocol.Opcode.Text, ByteString("Message 1"), fin = true)
@@ -92,30 +70,36 @@ class WebsocketClientSpec extends FreeSpec with Matchers with WithMaterializerSp
     }
   }
 
-  abstract class EstablishedConnectionSetup extends TestSetup {
-    expectWireData(
-      """GET /ws HTTP/1.1
-        |Upgrade: websocket
-        |Connection: upgrade
-        |Sec-WebSocket-Key: YLQguzhR2dR6y5M9vnA5mw==
-        |Sec-WebSocket-Version: 13
-        |Host: example.org
-        |User-Agent: akka-http/test
-        |
-        |""".stripMarginWithNewline("\r\n"))
+  def UpgradeRequestBytes = ByteString {
+    """GET /ws HTTP/1.1
+      |Upgrade: websocket
+      |Connection: upgrade
+      |Sec-WebSocket-Key: YLQguzhR2dR6y5M9vnA5mw==
+      |Sec-WebSocket-Version: 13
+      |Host: example.org
+      |User-Agent: akka-http/test
+      |
+      |""".stripMarginWithNewline("\r\n")
+  }
 
-    sendWireData(
-      """HTTP/1.1 101 Switching Protocols
-        |Upgrade: websocket
-        |Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
-        |Server: akka-http/test
-        |Connection: upgrade
-        |
-        |""")
+  def UpgradeResponseBytes = ByteString {
+    """HTTP/1.1 101 Switching Protocols
+      |Upgrade: websocket
+      |Sec-WebSocket-Accept: s3pPLMBiTxaQ9kYGzzhZRbK+xOo=
+      |Server: akka-http/test
+      |Connection: upgrade
+      |
+      |""".stripMarginWithNewline("\r\n")
+  }
+
+  abstract class EstablishedConnectionSetup extends TestSetup {
+    expectWireData(UpgradeRequestBytes)
+    sendWireData(UpgradeResponseBytes)
   }
 
   abstract class TestSetup extends WSTestSetupBase {
-    def clientImplementation: Flow[Message, Message, Unit]
+    protected def noMsgTimeout: FiniteDuration = 100.millis
+    protected def clientImplementation: Flow[Message, Message, Unit]
 
     val random = new Random(0)
     def settings = ClientConnectionSettings(system)
@@ -127,19 +111,21 @@ class WebsocketClientSpec extends FreeSpec with Matchers with WithMaterializerSp
       Http(system).websocketClientLayer(targetUri, settings, random)
 
     val (netOut, netIn) = {
-      val netOut = TestSubscriber.manualProbe[ByteString]
-      val netIn = TestPublisher.manualProbe[ByteString]()
+      val netOut = ByteStringSinkProbe()
+      val netIn = TestPublisher.probe[ByteString]()
 
       FlowGraph.closed(clientLayer) { implicit b ⇒
         client ⇒
           import FlowGraph.Implicits._
           Source(netIn) ~> Flow[ByteString].map(SessionBytes(null, _)) ~> client.in2
-          client.out1 ~> Flow[SslTlsOutbound].collect { case SendBytes(x) ⇒ x } ~> Sink(netOut)
+          client.out1 ~> Flow[SslTlsOutbound].collect { case SendBytes(x) ⇒ x } ~> netOut.sink
           client.out2 ~> clientImplementation ~> client.in1
       }.run()
 
       netOut -> netIn
     }
+    def expectBytes(length: Int): ByteString = netOut.expectBytes(length)
+    def expectBytes(bytes: ByteString): Unit = netOut.expectBytes(bytes)
 
     def wipeDate(string: String) =
       string.fastSplit('\n').map {
@@ -147,26 +133,17 @@ class WebsocketClientSpec extends FreeSpec with Matchers with WithMaterializerSp
         case s                          ⇒ s
       }.mkString("\n")
 
-    val netInSub = netIn.expectSubscription()
-    val netOutSub = netOut.expectSubscription()
-
-    def expectNextChunk(): ByteString = {
-      netOutSub.request(1)
-      netOut.expectNext()
-    }
-
     def sendWireData(data: String): Unit = sendWireData(ByteString(data.stripMarginWithNewline("\r\n"), "ASCII"))
-    def sendWireData(data: ByteString): Unit = netInSub.sendNext(data)
+    def sendWireData(data: ByteString): Unit = netIn.sendNext(data)
 
     def send(bytes: ByteString): Unit = sendWireData(bytes)
 
-    def expectWireData(s: String) = {
-      netOutSub.request(1)
-      netOut.expectNext().utf8String shouldEqual s.stripMarginWithNewline("\r\n")
-    }
+    def expectWireData(s: String) = netOut.expectUtf8EncodedString(s.stripMarginWithNewline("\r\n"))
+    def expectWireData(bs: ByteString) = netOut.expectBytes(bs)
+    def expectNoWireData() = netOut.expectNoBytes(noMsgTimeout)
 
     def expectNetworkClose(): Unit = netOut.expectComplete()
-    def closeNetworkInput(): Unit = netInSub.sendComplete()
+    def closeNetworkInput(): Unit = netIn.sendComplete()
   }
 
   trait ClientEchoes extends TestSetup {
@@ -174,11 +151,8 @@ class WebsocketClientSpec extends FreeSpec with Matchers with WithMaterializerSp
     def echoServer: Flow[Message, Message, Unit] = Flow[Message]
   }
   trait ClientProbes extends TestSetup {
-    lazy val messagesOut = TestPublisher.manualProbe[Message]()
-    lazy val messagesIn = TestSubscriber.manualProbe[Message]()
-
-    lazy val messagesOutSub = messagesOut.expectSubscription()
-    lazy val messagesInSub = messagesIn.expectSubscription()
+    lazy val messagesOut = TestPublisher.probe[Message]()
+    lazy val messagesIn = TestSubscriber.probe[Message]()
 
     override def clientImplementation: Flow[Message, Message, Unit] =
       Flow.wrap(Sink(messagesIn), Source(messagesOut))(Keep.none)
